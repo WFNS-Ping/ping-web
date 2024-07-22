f78d1a5460c9906dd713b32165873d2df58013c1
Jungsoo <sunpl13@naver.com>
2024-07-22 22:03:19 +0900

    feat: 지도 페이지 UI 수정

---
+++ app/map/MapPage.spec.tsx
@@ -0,0 +1,39 @@
+import userEvent from "@testing-library/user-event";
+import MapPage from "./page";
+import { screen, render } from "@testing-library/react";
+
+jest.mock("react-kakao-maps-sdk", () => ({
+  Map: () => <div data-testid="kakaoMap" />,
+}));
+
+jest.mock("../../hooks/useKaKaoLoader", () => jest.fn());
+const mockPushfn = jest.fn();
+jest.mock("../../hooks/useAppRouter", () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      ...jest.requireActual("../../hooks/useKaKaoLoader"),
+      push: mockPushfn,
+    };
+  });
+});
+describe("MapPage", () => {
+  beforeEach(() => {
+    render(<MapPage />);
+  });
+  it("카카오 맵이 띄워져야 한다.", () => {
+    const map = screen.getByTestId("kakaoMap");
+    expect(map).toBeInTheDocument();
+  });
+
+  it("검색 버튼이 있어야 한다.", () => {
+    const button = screen.getByRole("button", { name: "Ping 찍을 곳 검색" });
+    expect(button).toBeInTheDocument();
+  });
+
+  it("검색 버튼 클릭 시 검색 페이지로 이동되어야 한다.", async () => {
+    const button = screen.getByRole("button", { name: "Ping 찍을 곳 검색" });
+    await userEvent.click(button);
+
+    expect(mockPushfn).toHaveBeenCalledWith("/search");
+  });
+});
--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,24 +2,57 @@
 import useKakaoLoader from "@/hooks/useKaKaoLoader";
 import React from "react";
 import { Map } from "react-kakao-maps-sdk";
-useKakaoLoader;
+import { SearchIcon } from "@/assets/icons";
+import { cn } from "@/utils/cn";
+import useAppRouter from "@/hooks/useAppRouter";
+
 const MapPage = () => {
   useKakaoLoader();
+  const router = useAppRouter();
   return (
-    <Map // 지도를 표시할 Container
-      id="map"
-      center={{
-        // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
-      }}
-      style={{
-        // 지도의 크기
-        width: "100%",
-        height: "100%",
-      }}
-      level={3} // 지도의 확대 레벨
-    />
+    <main className="relative w-full h-full p-0">
+      <Map // 지도를 표시할 Container
+        id="map"
+        center={{
+          // 지도의 중심좌표
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+        style={{
+          // 지도의 크기
+          width: "100%",
+          height: "100%",
+        }}
+        level={3} // 지도의 확대 레벨
+      />
+      <div className="px-2 absolute top-2 z-10 w-full">
+        <button
+          className={cn(
+            "input-base",
+            "px-6",
+            "py-4",
+            "w-full",
+            "description",
+            "font-normal",
+            "text-black-200",
+            "text-left",
+            "bg-white",
+            "shadow-[0px_2px_2px_0px_rgba(0,0,0,0.2)]"
+          )}
+          type="button"
+          onClick={() => router.push("/search")}
+        >
+          Ping 찍을 곳 검색
+          <SearchIcon
+            aria-label="search-button"
+            role="button"
+            width="1.5rem"
+            height="1.5rem"
+            className={cn("absolute", "top-4", "right-6", "text-black-950")}
+          />
+        </button>
+      </div>
+    </main>
   );
 };
 
